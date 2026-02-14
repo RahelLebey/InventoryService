8afcfa81743855bc081a94477538c07cfc203f6b
Rahel <onedayr299@gmail.com>
2026-02-13 19:07:27 -0500

    Lab 4 Part 2 - MVC consumes Maintenance API

---
+++ InventoryService.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace InventoryService.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ InventoryService.MVC/Views/Maintenance/History.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace InventoryService.MVC.Views.Maintenance
+{
+    public class HistoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
